292c4b655917e98b216e5eb04d9d9a167a26237e
komal kumar <152193731+komalkumarpalwai@users.noreply.github.com>
2024-07-12 18:37:56 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const path = require('path');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-const connectDB = require('./connectDB'); // Import the connectDB function
+const connectDB = require('./connectDB'); 
 const User = require('./models/user'); 
 const Post = require('./models/post'); 
 require('dotenv').config();
@@ -11,28 +11,22 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); 
 
-// Use express-session middleware
 app.use(session({
     secret: 'mySuperSecretKey123!@#',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true } // Set to true only if using HTTPS
+    cookie: { secure: true }
 }));
 
-// Connect to MongoDB
 connectDB().catch(console.dir);
 
-// Multer setup
 const upload = multer({ dest: 'uploads/' });
 
-// Routes for API
-// Get all posts
 app.get('/api/posts', async (req, res) => {
     try {
         const posts = await Post.find();
@@ -67,7 +61,6 @@ app.post('/api/post', upload.single('image'), async (req, res) => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Routes for web pages
 const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
 const feedRouter = require('./routes/feed');
@@ -75,8 +68,7 @@ const feedRouter = require('./routes/feed');
 app.use('/', registerRouter);
 app.use('/login', loginRouter);
 app.use('/feed', feedRouter);
-
-// Error handling 
+g 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Internal Server Error');
